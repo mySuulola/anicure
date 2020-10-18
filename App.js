fd37ea06a8ba331b7bc59d0744e8c4221a3c054f
Oluwaseyi SUULOLA <oluwaseyisuulola@gmail.com>
2020-10-18 09:44:13 +0100

    had to delete the commit history

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen'
+import Providers from './src/navigation';
+
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+  return (
+   <>
+      <Providers />
+    </>
+   );
+};
+
+
+export default App;
